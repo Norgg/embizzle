b2ea0c83b9e07b5cac023448cb1412db29506b13
Norgg <groups@norgg.org>
2015-08-22 15:25:48 +0100

    More population modelling.

--- embizzle/models.py
+++ embizzle/models.py
@@ -1,14 +1,28 @@
 from django.contrib.auth.models import User
-from django.db.models import CharField, ForeignKey, IntegerField, Model, OneToOneField
+from django.db.models import CharField, DateTimeField, FloatField, ForeignKey, IntegerField, Model, OneToOneField
+from django.utils import timezone
+
+
+class Game(Model):
+    last_tick = DateTimeField(default=timezone.now)
 
 
 class Civilisation(Model):
     name = CharField(max_length=1024)
-    funds = IntegerField()
-    population = IntegerField()
+    funds = IntegerField(default=1000)
+    unrest = IntegerField(default=0)  # How unhappy the population are with their leader
+    children = IntegerField(default=100)  # Number of children in the population
+    breeders = IntegerField(default=100)  # People of breeding age
+    others = IntegerField(default=100)  # Others
+    birth_rate = FloatField(default=0.001)  # birth rate per thousand people
+    death_rate = FloatField(default=0.002)  # death rate per thousand people
+
+    def population(self):
+        return self.children + self.breeders + self.others
 
 
 class Leader(Model):
+    game = ForeignKey(Game)
     user = ForeignKey(User)
     civ = OneToOneField(Civilisation)
     funds = IntegerField()
--- embizzle/views.py
+++ embizzle/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from models import Civilisation, Leader
+
 
 def index(request):
-    return render(request, "index.html")
+    civ = Civilisation(name="Norggland")
+    leader = Leader(civ=civ, funds=2000)
+    return render(request, "index.html", locals())
+
+
+def login(request):
+    pass
+
+
+def register(request):
+    pass
