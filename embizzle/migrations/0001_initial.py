c86cd1a679c1cc8af2dd650297f1c1b04154ca83
Norgg <groups@norgg.org>
2015-08-22 16:59:06 +0100

    Reset migrations.

--- embizzle/migrations/0001_initial.py
+++ embizzle/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.utils.timezone
 from django.conf import settings
 
 
@@ -16,18 +17,37 @@ class Migration(migrations.Migration):
             name='Civilisation',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=1024)),
-                ('funds', models.IntegerField()),
-                ('population', models.IntegerField()),
+                ('name', models.CharField(default=b'Nowhere', max_length=1024)),
+                ('funds', models.IntegerField(default=1000)),
+                ('unrest', models.IntegerField(default=0)),
+                ('children', models.IntegerField(default=0)),
+                ('breeders', models.IntegerField(default=100)),
+                ('others', models.IntegerField(default=20)),
+                ('birth_rate', models.FloatField(default=0.04)),
+                ('children_death_rate', models.FloatField(default=0.005)),
+                ('breeder_death_rate', models.FloatField(default=0.01)),
+                ('other_death_rate', models.FloatField(default=0.05)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Game',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('started_at', models.DateTimeField(default=django.utils.timezone.now)),
+                ('last_tick', models.DateTimeField(default=django.utils.timezone.now)),
+                ('ticks', models.IntegerField(default=0)),
+                ('tick_length', models.IntegerField(default=5)),
             ],
         ),
         migrations.CreateModel(
             name='Leader',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('funds', models.IntegerField()),
-                ('civ', models.OneToOneField(to='embizzle.Civilisation')),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('name', models.CharField(default=b'Someone', max_length=1024)),
+                ('funds', models.IntegerField(default=100)),
+                ('civ', models.OneToOneField(related_name='leader', to='embizzle.Civilisation')),
+                ('game', models.ForeignKey(related_name='leaders', to='embizzle.Game')),
+                ('user', models.ForeignKey(related_name='leaders', to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
