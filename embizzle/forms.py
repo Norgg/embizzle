a886373df67a049c7788e7b3cfab303e6dfdcdab
Norgg <groups@norgg.org>
2015-08-22 21:18:35 +0100

    User registration.

--- embizzle/forms.py
+++ embizzle/forms.py
@@ -1,7 +1,17 @@
-from django.forms import CharField, Form, PasswordInput
+from django.contrib.auth.models import User
+from django.forms import CharField, Form, PasswordInput, ValidationError
 
 
 class RegistrationForm(Form):
     username = CharField()
+    leader_name = CharField()
+    civilisation_name = CharField()
     password = CharField(widget=PasswordInput())
     repeat_password = CharField(widget=PasswordInput())
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        if User.objects.filter(username=cleaned_data['username']).count() > 0:
+            raise ValidationError("Username taken.")
+        elif cleaned_data['password'] != cleaned_data['repeat_password']:
+            raise ValidationError("Passwords do not match.")
--- embizzle/views.py
+++ embizzle/views.py
@@ -2,8 +2,10 @@ import __init__
 
 import actions
 
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
+from django.db import transaction
 from django.shortcuts import redirect, render
 
 from forms import RegistrationForm
@@ -11,44 +13,51 @@ from forms import RegistrationForm
 from models import Civilisation, Game, Leader
 
 
-def test_objs():
-    user, _ = User.objects.get_or_create(id=1, username="Norgg")
-    game, _ = Game.objects.get_or_create(id=1)
-    civ, _ = Civilisation.objects.get_or_create(id=1, name="Norggland")
-    leader, _ = Leader.objects.get_or_create(id=1, name="Norgg", game=game, civ=civ, user=user)
-    return user, game, civ, leader
-
-
 @login_required
 def index(request):
-    user, game, civ, leader = test_objs()
+    user = request.user
+    leader = user.leaders.all()[0]
+    civ = leader.civ
+    game = leader.game
     game.check_tick()
     # game.process_ticks(1000)
     return render(request, "index.html", locals())
 
 
-def login(request):
-    pass
-
-
+@transaction.atomic
 def register(request):
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            print(form.data['password'])
+            game, created = Game.objects.get_or_create(id=1)  # TODO: Handle game creation properly.
+            user = User.objects.create(username=form.cleaned_data['username'])
+            civ = Civilisation.objects.create(name=form.cleaned_data['civilisation_name'])
+            leader = Leader.objects.create(name=form.cleaned_data['leader_name'], user=user, civ=civ, game=game)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            login(request, user)
+            print("User created.")
+            return redirect(index)
     else:
         form = RegistrationForm()
     return render(request, "register.html", locals())
 
 
+@login_required
 def action(request, action):
+    user = request.user
+    leader = user.leaders.all()[0]
+    civ = leader.civ
+    game = leader.game
     added_keys = set(actions.__dict__.keys()) - set(__init__.__dict__.keys())
-    user, game, civ, leader = test_objs()
     if action in added_keys:
         actions.__dict__[action](user, game, civ, leader)
     return redirect(index)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def reset(request):
     Leader.objects.all().delete()
     Civilisation.objects.all().delete()
